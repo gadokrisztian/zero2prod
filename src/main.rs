130eff155f73166dfcb78fb30f045e94cc2fcc4c
gadokrisztian <85160134+gadokrisztian@users.noreply.github.com>
2023-04-24 19:19:44 +0200

    dbconnection added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::run;
@@ -7,6 +8,9 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(&address).expect("Failed to bind port.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     println!("Listening on http://{}", listener.local_addr().unwrap());
-    run(listener)?.await
+    run(listener, connection)?.await
 }
