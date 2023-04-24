773bd99a4acb817234113a008efc53729d8d5103
gadokrisztian <85160134+gadokrisztian@users.noreply.github.com>
2023-04-24 19:46:08 +0200

    subscriptions implemented with connection pool and tests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::{Connection, PgConnection};
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::run;
@@ -8,9 +8,9 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(&address).expect("Failed to bind port.");
-    let connection = PgConnection::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
     println!("Listening on http://{}", listener.local_addr().unwrap());
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
