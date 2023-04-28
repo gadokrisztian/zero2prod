1de1afc8478859a9acf3fdfe8ecbfb70758d339c
gadokrisztian <85160134+gadokrisztian@users.noreply.github.com>
2023-04-28 16:44:24 +0200

    logging added

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::dispatcher::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    LogTracer::init().expect("Failed to set logger.");
+
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    set_global_default(subscriber.into()).expect("Failed to set subscriber.");
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(&address).expect("Failed to bind port.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
-    println!("Listening on http://{}", listener.local_addr().unwrap());
+    // println!("Listening on http://{}", listener.local_addr().unwrap());
     run(listener, connection_pool)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use actix_web::middleware::Logger;
 
 use crate::routes::{health_check, subscibe};
 
@@ -9,6 +10,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscibe))
             .app_data(db_pool.clone())
